7d977808dca31bd937cf210286c0692d4e04a097
Jane Hoffswell <jhoffs@cs.washington.edu>
2020-03-10 13:59:03 -0700

    Buggy: Prototype Behavior of Photo Conference Map
    Trying to get the conference map to show images as the background for each of the countries. Behavior does not currently seem supported by Vega and have not yet figured out a workaround.

--- website/conferencemap.js
+++ website/conferencemap.js
@@ -1,4 +1,5 @@
 var container, loader, runtime, spec, view;
+var visited = [];
 
 // Show the page that contains the conference map
 function showConferenceMap() {
@@ -47,7 +48,10 @@ function updateMap(data) {
 
 // Visualize the map using Vega and handle interactions
 function visualize(spec) {
-  if (view) view.finalize(); // finalize existing view
+  if(view) {
+    view.finalize(); // finalize existing view
+    return;
+  }
   view = new vega.View(runtime = vega.parse(spec));
   view.logLevel(vega.Warn)
     .renderer("svg")
@@ -55,7 +59,53 @@ function visualize(spec) {
     .hover()
     .run();
 
+  setPaths();
+
+  view.addSignalListener("point", function(name, data) {
+    setPaths();
+  });
+
   view.addSignalListener("selected", function(name, data) {
     if(data.tab) openTab(null, data.tab);
-  })
+  });
+
+  view.addSignalListener("showTravel", function(name, data) {
+    getPaths();
+  });
+}
+
+function getPaths() {
+  if(visited.length > 0) return setPaths();
+
+  // Get the paths
+  var marks = document.getElementById("conference-map").getElementsByTagName("path");
+  for (var i = 0; i < marks.length; i++) {
+    var mark = marks[i];
+    if(!mark.__data__) {
+      continue;
+    } else {
+      var datum = mark.__data__.datum;
+      if(datum && datum.type && datum.type == "Feature") {
+        if(datum.year) visited.push(mark.__values__.d);
+      }
+    }
+  }
+
+  console.log("visited:", visited)
+  if(visited.length > 0) setPaths();
+}
+
+function setPaths() {
+  console.log("setting them...")
+  var signals = ["canadaPath", "costaRicaPath", "mexicoPath"];
+  var clips = document.getElementsByTagName("clipPath");
+  var startingPoint = 7;
+  for (var i = 0; i < clips.length; i++) {
+    view.signal(signals[i], visited[i]);
+    spec.signals[7+i].value = visited[i];
+    clips[i].innerHTML = "<path d='" + visited[i] + "'></path>";
+  }
+  // view.signal("mexicoPath", visited[1]);
+  visualize(spec);
+  
 }
